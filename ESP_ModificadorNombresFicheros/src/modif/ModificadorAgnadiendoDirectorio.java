77ad322b80458f7e8a52931bdfd9b1cabb86af5b
koldo <koldo@SHIRKA>
2018-08-06 19:20:47 +0200

    Ahora sí funciona el preguntar dónde añadir el nombre del directorio

--- ESP_ModificadorNombresFicheros/src/modif/ModificadorAgnadiendoDirectorio.java
+++ ESP_ModificadorNombresFicheros/src/modif/ModificadorAgnadiendoDirectorio.java
@@ -33,13 +33,25 @@ public class ModificadorAgnadiendoDirectorio {
 		}
 		// Busca los originales y los guarda en la lista "originales"
 		this.buscarOriginales();
-		// Crea los modificados a partir de la lista de originales
-		this.crearModificados();
 		
-		this.iniciarProceso();
+		// Crea los modificados a partir de la lista de originales:		
+		// Define opciones de botones personalizadas
+		Object[] opciones = {"Delante", "Detrás", "Cancelar"};			
+		// Muestra un dialogo y pregunta
+		int respuesta = JOptionPane.showOptionDialog(this.app, "¿Dónde quieres añadir el nombre del directorio?", "Posición del directorio", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[1]);
+		if (respuesta!=2) {
+			// Si no se ha cancela la acción, crea los modificados e inicia el proceso de renombramiento
+			this.crearModificados(respuesta);
+		
+			// Inicia el proceso de renombrado
+			this.iniciarProceso();
+		}
 	}
 
-	private void crearModificados() {
+	private void crearModificados(int posicion) {
+		// posicion = 0 ADELANTE
+		// posicion = 1 ATRAS
+		
 		Iterator<File> it = this.originales.iterator();
 		while (it.hasNext()) {
 			File original = (File) it.next();
@@ -57,15 +69,12 @@ public class ModificadorAgnadiendoDirectorio {
 				// Se ejecuta este código si el nombre del directorio padre NO es un TimeStamp
 				// En este caso se pregunta qué hacer, si añadir al principio o al final
 				
-				// Opciones de botones personalizadas
-				Object[] opciones = {"Delante", "Detrás"};
-				int respuesta = JOptionPane.showOptionDialog(this.app, "¿Dónde quieres añadir el nombre del directorio?", "Posición del directorio", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[1]);
 				
-				if (respuesta == 0) {
+				if (posicion == 0) {
 					// En este caso, se añade al principio
 					nombreNuevo = padre + "_" + nombre.substring(0, posPunto) + "." + nombre.substring(posPunto + 1);
 					
-				} else if (respuesta == 1) {
+				} else if (posicion == 1) {
 					// En este caso, se añade al final
 					nombreNuevo = nombre.substring(0, posPunto) + "_" + padre + "." + nombre.substring(posPunto + 1);
 
